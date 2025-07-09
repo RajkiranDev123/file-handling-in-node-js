2dac84273a412b857e3b67cee295f2430a075897
raj <rajtech645@gmail.com>
2025-07-09 17:57:26 +0530

    file handling in node js

---
+++ server.js
@@ -0,0 +1,77 @@
+import dotenv from "dotenv"
+dotenv.config({ path: "./.env" })
+import express from "express"
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+import fs from "fs"
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const app = express()
+const PORT = process.env.PORT
+console.log("port no ==> ", PORT)
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+app.get("/", (req, res) => {
+
+    //sync
+
+    fs.writeFileSync("./sample.txt", "1")
+    fs.writeFileSync("./sample.txt", "2")// over written the previous : 2
+
+
+    //async
+    fs.writeFile("./sample.txt", "3 ", (err) => {
+        //It only returns err in the callback.
+        if (err) {
+            console.error("Error writing file:", err);
+        } else {
+            const result = fs.readFileSync("./sample.txt", "utf-8")
+            console.log("result of synchronously reading 1==>", result)
+
+            //append
+
+            fs.appendFileSync("./sample.txt", new Date().getDate().toLocaleString())
+
+            //copy
+            fs.cpSync("./sample.txt","./sample2.txt")
+
+            //delete
+            // fs.unlinkSync("./sample2.txt")
+
+            //make directory
+
+            fs.mkdirSync("a/b",{recursive:true})
+        }
+    })
+
+
+    // read
+
+    const result = fs.readFileSync("./sample.txt", "utf-8")
+    console.log("result of synchronously reading==>", result)
+
+    // asynchronously reading : expects a cb
+
+    fs.readFile("./sample.txt", "utf-8", (err, data) => {
+        //It only returns err in the callback.
+        if (err) {
+            console.error("Error reading file:", err);
+        } else {
+
+            console.log("data from async read ==>", data)
+        }
+    })
+
+
+
+
+})
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT} port no!`)
+})
+
+
